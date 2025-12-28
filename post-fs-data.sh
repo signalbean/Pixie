2f4b45fa45270493e2ff217a170f176ef68f3639
signalbean <signalbean@gmail.com>
2025-12-28 14:41:23 +0530

    revert: brand naming for spoof

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -2,6 +2,7 @@
 MODDIR=${0%/*}
 
 # Spoof device as Pixel XL globally
+resetprop ro.product.brand google
 resetprop ro.product.manufacturer Google
 resetprop ro.product.model "Pixel XL"
 resetprop ro.product.device marlin
