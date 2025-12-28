dd85a66657993bf821e6478a33a8c7a4d772067d
signalbean <signalbean@gmail.com>
2025-12-29 02:35:27 +0530

    refactor: update to v1.5 with simplified installation
    - Bump version to 1.5 (versionCode 15)
    - Clean up update-binary script, remove verbose comments
    - Simplify customize.sh with minimal configuration
    - Remove post-fs-data.sh and uninstall.sh (no longer needed)
    - Switch to PROPFILE=true for cleaner property handling

--- customize.sh
+++ customize.sh
@@ -1,43 +1,17 @@
-##########################################################################################
-#
-# Magisk Module Installer Script
-#
-##########################################################################################
-
-##########################################################################################
-# Config Flags
-##########################################################################################
-
-SKIPMOUNT=false
-PROPFILE=false
-LATESTARTSERVICE=false
-POSTFSDATA=true
-
-##########################################################################################
-# Install
-##########################################################################################
+PROPFILE=true
 
 print_modname() {
-  ui_print "*******************************"
-  ui_print "             Pixie             "
-  ui_print "*******************************"
+  ui_print "*********************"
+  ui_print "        Pixie        "
+  ui_print "*********************"
 }
 
 on_install() {
   ui_print "- Extracting module files"
   unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
-  
-  ui_print "- Setting permissions"
-  set_perm_recursive $MODPATH 0 0 0755 0644
-  set_perm $MODPATH/post-fs-data.sh 0 0 0755
-  
   ui_print "- Module installed successfully"
-  ui_print "- Device: Pixel XL (marlin)"
-  ui_print "- Method: sysconfig + property spoofing"
 }
 
 set_permissions() {
-  set_perm_recursive $MODPATH 0 0 0755 0644
-  set_perm $MODPATH/post-fs-data.sh 0 0 0755
-  set_perm_recursive $MODPATH/system/product/etc/sysconfig 0 0 0755 0644
+  set_perm_recursive $MODPATH/system 0 0 0755 0644
 }
--- uninstall.sh
+++
@@ -1,6 +0,0 @@
-#!/system/bin/sh
-
-# Uninstall script for Pixie module
-
-ui_print "Pixie module removed."
-ui_print "Please reboot your device"
